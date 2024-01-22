7a75bfd7a6ee2c1cc4c5674b446d63a36d4cc50b
💝 🎀  𝒞𝐻💙𝒫𝑅𝒜  🎀  💝 <you@example.com>
2024-01-22 22:32:26 +0530

    development

---
+++ app/src/main/java/com/michelbchopra/crystacashbook/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.michelbchopra.crystacashbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
